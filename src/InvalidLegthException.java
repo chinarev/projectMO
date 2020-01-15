87aae4a1362b74b58d9d819d9287809aca982a5d
Mike <mihaelchinarev@gmail.com>
2020-01-16 01:28:27 +0400

    Initial commit

---
+++ src/InvalidLegthException.java
@@ -0,0 +1,9 @@
+
+@SuppressWarnings("serial")
+public class InvalidLegthException extends RuntimeException {
+
+    public InvalidLegthException() {
+        super("Piece size exceeds the stock size");
+    }
+
+}
---
+++ src/InvalidParameterException.java
@@ -0,0 +1,9 @@
+
+@SuppressWarnings("serial")
+public class InvalidParameterException extends RuntimeException {
+
+    public InvalidParameterException() {
+        super("Array length mismatch error. Recheck the parametrs");
+    }
+
+}
